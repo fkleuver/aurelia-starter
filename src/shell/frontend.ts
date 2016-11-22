35377769a589c88554df4b06de02cf719c689862
Fred Kleuver <fred@avurad.nl>
2016-11-22 23:04:27 +0100

    Add another dropdown for demo purposes

--- src/shell/frontend.ts
+++ src/shell/frontend.ts
@@ -1,8 +1,20 @@
 export class Frontend {
-    options = [
+    optionsFew = [
         { displayName: "Foo 1", value: 1 },
-        { displayName: "Bar 2", value: 2 },
-        { displayName: "Baz 3", value: 3 },
-        { displayName: "Qux 4", value: 4 }
+        { displayName: "Bar 1", value: 2 },
+        { displayName: "Baz 2", value: 3 },
+        { displayName: "Qux 2", value: 4 }
+    ];
+    optionsMany = [
+        { displayName: "Foo 1", value: 1 },
+        { displayName: "Bar 1", value: 2 },
+        { displayName: "Baz 2", value: 3 },
+        { displayName: "Qux 2", value: 4 },
+        { displayName: "Quux 1", value: 5 },
+        { displayName: "Quuz 1", value: 6 },
+        { displayName: "Corge 2", value: 7 },
+        { displayName: "Grault 2", value: 8 },
+        { displayName: "Garply 1", value: 9 },
+        { displayName: "Waldo 1", value: 10 }
     ];
 }
