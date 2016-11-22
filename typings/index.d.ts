716ee45ce464da1afb8a423999b2d03fe2bd94cf
Fred Kleuver <fred@avurad.nl>
2016-11-22 15:49:43 +0100

    Added/updated dev typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,2 +1,4 @@
 /// <reference path="globals/jasmine/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
+/// <reference path="globals/require/index.d.ts" />
+/// <reference path="globals/wallabyjs/index.d.ts" />
